345036db05375239b41ca838bbc7624254f48097
Milan <gajeramilan007@gmail.com>
2026-08-24 21:17:53 +0530

    - Change package name.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.advick.krishimark_company"
+    namespace = "com.advick.ifarmconnect"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.advick.krishimark_company"
+        applicationId = "com.advick.ifarmconnect"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -1,3 +1,3 @@
 class AppConstants {
-  static String appName = 'Krishi Mart';
+  static String appName = 'iFarm Connect';
 }
